864c12367945ee3e579e24bd4cffd2f4830829c0
jeeembx909 <102240049+jeeembx909@users.noreply.github.com>
2022-03-23 16:59:00 +0200

    Create crypt.js
    test

---
+++ crypt.js
@@ -0,0 +1,46 @@
+1 |function Block(letter, rate, used, father, code) {
+eB this. letter = letter;
+
+5 See ese
+
+4 | this.used = used;
+
+5 Re ear
+
+G See ee a
+
+7
+
+Ey
+
+9 Function FindCode(block) {
+
+10
+
+i Sea @ae Cre secre a
+
+cP) Eyres ea re Ser a
+cE }
+
+cr Prone
+
+cL Peano tra ae tae eed eas ee
+16 yaaa
+
+a
+
+18 else if (block.letter == tree[preminIndex].letter) {
+rT) block.code = ‘1°;
+
+ox)
+
+pa rong
+
+22 FindCode(tree[block.father]) ;
+
+PE] Dye ee Rr eae
+pz
+
+PE
+
+1
